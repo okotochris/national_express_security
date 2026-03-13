737458d4d84d693d5d2dab7f361ca9b61812bcc8
okoto.chris <okoto.chis.oc@gmail.com>
2026-03-13 11:40:55 +0100

    footer updated

--- frontend/app/[locale]/component/footer.tsx
+++ frontend/app/[locale]/component/footer.tsx
@@ -6,7 +6,7 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'next/navigation';
 
 export default function Footer() {
-  const [selectedCountry, setSelectedCountry] = useState("United Kingdom");
+  const [selectedCountry, setSelectedCountry] = useState("United States");
   const [states, setStates] = useState(countries[0].states);
   const [isLoading, setIsLoading] = useState(false); // Simple loading state
 
